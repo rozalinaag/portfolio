4f219ad0f89afb661e15c83198a13c49cdb6108c
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-10-11 18:26:20 -0500

    router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,30 @@
 import './App.css';
 import { createTheme, MantineProvider } from '@mantine/core';
 import Home from '../pages/Home/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from 'react';
+import NotFound from '../pages/NotFound/NotFound';
 
 const theme = createTheme({
   fontFamily: 'Montserrat, sans-serif',
   defaultRadius: 'md',
 });
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+]);
+
 function App() {
   return (
-    <MantineProvider theme={theme}>
-      <Home />
-    </MantineProvider>
+    <React.StrictMode>
+      <MantineProvider theme={theme}>
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </React.StrictMode>
   );
 }
 
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function NotFound() {
+  return (
+    <div>
+      not found go home
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
