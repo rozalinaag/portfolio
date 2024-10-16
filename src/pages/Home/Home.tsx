53027d46bd6715b008db0d7991020511fcaade33
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-10-16 15:30:29 -0500

    description

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,21 @@
 import Layout from '../../app/Layout/Layout';
-import Header from '../../components/Header/Header';
+import css from './styles.module.css';
 
 export default function Home() {
   return (
     <Layout>
-      <div></div>
+      <div>
+        <div className={css.title}>Hi, my name is</div>
+        <div className={css.name}>Rozalina Agisheva.</div>
+        <div className={css.whatToDo}>I build things for the web.</div>
+        <div className={css.description}>
+          I’m a software engineer specializing in creating user-friendly
+          websites. For more than three years, I worked at Magnit, a large
+          retail company, where I developed two corporate portals. My bachelor's
+          degree in Computer Science has significantly contributed to my work.
+          Web development is my passion, and I truly enjoy what I do.
+        </div>
+      </div>
     </Layout>
   );
 }
