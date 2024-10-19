431a61400839c2875328cab77df1795aff5351d3
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-10-18 20:42:34 -0500

    footer

--- src/app/Layout/Layout.tsx
+++ src/app/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import classNames from 'classnames';
 import Header from '../../components/Header/Header';
 import css from './styles.module.css';
+import Footer from '../../components/Footer/Footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={css.wrapper}>
       <Header />
       <div className={classNames('content', css.page)}>{children}</div>
+      <Footer />
     </div>
   );
 }
