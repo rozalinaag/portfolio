530a25c4e0a8daa89cd890570ff547ddd5c8be89
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-10-20 23:25:53 -0500

    project page

--- src/pages/Projects/Project/Project.tsx
+++ src/pages/Projects/Project/Project.tsx
@@ -1,8 +1,25 @@
 import { useParams } from 'react-router-dom';
 import Layout from '../../../app/Layout/Layout';
+import { useEffect } from 'react';
 
 export default function Project() {
   const { projectId } = useParams();
-  console.log(projectId);
-  return <Layout>{projectId} AALALLAL</Layout>;
+
+  useEffect(() => {
+    //getting project by Id
+  }, []);
+
+  return (
+    <Layout>
+      <div>
+        <div>
+          {projectId}
+          <div>Name of project </div>
+          <div>Description</div>
+        </div>
+
+        <a>link to deploy</a>
+      </div>
+    </Layout>
+  );
 }
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,5 +1,29 @@
 import Layout from '../../app/Layout/Layout';
 
 export default function Projects() {
-  return <Layout>projects</Layout>;
+  return (
+    <Layout>
+      <div>
+        Projects:
+        <div>
+          <div>
+            <div>
+              <div>Name of project </div>
+              <div>Description</div>
+            </div>
+
+            <a>link to deploy</a>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>Name of project </div>
+            <div>Description</div>
+          </div>
+
+          <a>link to deploy</a>
+        </div>
+      </div>
+    </Layout>
+  );
 }
