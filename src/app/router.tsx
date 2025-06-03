81ef9483c697eb0e0648cfe2d89830f9cbf78fc7
rozalinaaag <rosalina2000@mail.ru>
2025-06-03 18:20:45 -0400

    fix router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import NotFound from '../pages/NotFound/NotFound';
 import Projects from '../pages/Home/Projects/Projects';
-import ProjectCard from '../pages/Home/Projects/ProjectCard/ProjectCard.tsx';
 
 export const router = createBrowserRouter([
   {
@@ -13,9 +12,5 @@ export const router = createBrowserRouter([
   {
     path: '/projects',
     element: <Projects />,
-  },
-  {
-    path: '/projects/:projectId',
-    element: <ProjectCard />,
-  },
+  }
 ]);
